5671b3770df8b217781b9a87be9cef05c7aede72
quicksilver812 <allen.20211cse0227@presidencyuniversity.in>
2024-06-15 23:20:43 +0530

    added delete context functionality

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -1,11 +1,34 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const WorkoutContext = createContext()
 
+export const workoutsReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_WORKOUTS':
+            return {
+                workouts: action.payload
+            }
+            case 'CREATE_WORKOUTS':
+                return {
+                    workouts: [action.payload, ...state.workouts]
+                }
+                case 'DELETE_WORKOUTS':
+                    return {
+                        workouts: state.workouts.filter((w)=>w._id!==action.payload._id)
+                    }
+            default:
+                return state
+    }
+}
+
 export const WorkoutContextProvider = ({children}) => {
+    const [state, dispatch] = useReducer(workoutsReducer, {
+        workouts: null
+    })
+
 
     return(
-        <WorkoutContext.Provider>
+        <WorkoutContext.Provider value={{...state, dispatch}}> 
             {children}
         </WorkoutContext.Provider>
     )
